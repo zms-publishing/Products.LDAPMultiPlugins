4d4bbc42e3bcb767549b63cb1d1b93bf6e3be1b6
Jens Vagelpohl <jens@dataflake.org>
2008-12-27 22:06:09 +0000

    - replace import from Globals
    git-svn-id: file:///svn-public/Products.LDAPMultiPlugins/trunk@1689 835909ba-7c00-0410-bfa4-884f43845301

--- Products/LDAPMultiPlugins/ActiveDirectoryMultiPlugin.py
+++ Products/LDAPMultiPlugins/ActiveDirectoryMultiPlugin.py
@@ -22,9 +22,9 @@ from urllib import quote_plus
 
 # Zope imports
 from Acquisition import aq_base
-from Globals import InitializeClass
-from Globals import DTMLFile
-from Globals import package_home
+from App.class_init import default__class_init__ as InitializeClass
+from App.Common import package_home
+from App.special_dtml import DTMLFile
 from AccessControl import ClassSecurityInfo
 from Products.LDAPUserFolder import manage_addLDAPUserFolder
 from Products.LDAPUserFolder.LDAPDelegate import filter_format
--- Products/LDAPMultiPlugins/LDAPPluginBase.py
+++ Products/LDAPMultiPlugins/LDAPPluginBase.py
@@ -23,7 +23,7 @@ import logging
 from AccessControl import ClassSecurityInfo
 from AccessControl.SecurityManagement import getSecurityManager
 from Acquisition import aq_base
-from Globals import InitializeClass
+from App.class_init import default__class_init__ as InitializeClass
 from OFS.Cache import Cacheable
 from OFS.Folder import Folder
 
